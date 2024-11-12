2349e124ca073cbb00ea774a75e0bd00d76d390e
cqz623 <qizhenchen063@gmail.com>
2024-11-12 18:38:33 -0500

    11.12

---
+++ Exercise/week9/scrip.js
@@ -0,0 +1,16 @@
+alert("Hello World")
+
+/*
+let num01 = 10
+let num02 = 20
+*/
+
+let num01 = 10
+let num02 = 20
+
+console. log(num01, num02)
+document.write("Hello World")
+
+var width;
+width = 40
+
